7665fee23cc56ba7fb718431e13c6f25979344c7
Prateek Obireddy <prateekobireddy@192-168-1-103.tpgi.com.au>
2020-05-02 17:20:57 +1000

    Safety commit - working on form

--- src/components/Enquire.vue
+++ src/components/Enquire.vue
@@ -4,8 +4,21 @@
       <div class="chevron jump text-center">
         <i class="fas fa-chevron-up"></i>
       </div>
-
       <p class="title text-center">Enquire</p>
+      <div class="form">
+        <form action="submit">
+          <div class="form-group">
+            <input type="text" class="name form-control text-center" placeholder="name" />
+          </div>
+          <div class="form-group">
+            <input type="email" class="name form-control text-center" placeholder="email" />
+          </div>
+          <div class="form-group">
+            <textarea class="form-control" rows="9"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -19,10 +32,29 @@ export default {
 <style lang="scss" scoped>
 .enquire {
   height: 100vh;
+  font-family: "Comic Sans MS";
   color: #6eb4c7;
+  .chevron {
+    margin-top: 20px;
+  }
   .title {
     font-size: 40px;
-    font-family: "Comic Sans MS";
+  }
+  .form {
+    padding: 50px 300px;
+    background-color: #e0ebe8;
+    .name {
+      border: 2px solid #6eb4c7;
+      border-radius: 50px;
+      height: 50px;
+      &::placeholder {
+        color: #6eb4c7;
+      }
+    }
+    textarea {
+      border: 2px solid #6eb4c7;
+      border-radius: 30px;
+    }
   }
 }
 </style>
