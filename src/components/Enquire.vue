48cd838cd209a72f9a0d5a489f7eb49f493fada6
Prateek Obireddy <prateekobireddy@192-168-1-103.tpgi.com.au>
2020-05-06 19:24:35 +1000

    Safety commit - working on email

--- src/components/Enquire.vue
+++ src/components/Enquire.vue
@@ -5,18 +5,33 @@
         <i class="fas fa-chevron-up"></i>
       </div>
       <p class="title text-center">Enquire</p>
-      <div class="form">
-        <form action="submit">
+      <div class="form text-center">
+        <form action="submit" @submit.prevent="sendEmail">
           <div class="form-group">
-            <input type="text" class="name form-control text-center" placeholder="name" />
+            <input
+              type="text"
+              class="name form-control text-center"
+              placeholder="name"
+              name="user_name"
+            />
           </div>
           <div class="form-group">
-            <input type="email" class="name form-control text-center" placeholder="email" />
+            <input
+              type="email"
+              class="name form-control text-center"
+              placeholder="email"
+              name="user_email"
+            />
           </div>
           <div class="form-group">
-            <textarea class="form-control" rows="9"></textarea>
+            <textarea
+              class="form-control text-center"
+              placeholder="Comment"
+              rows="9"
+              name="message"
+            ></textarea>
           </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
@@ -24,8 +39,32 @@
 </template>
 
 <script>
+import emailjs from "emailjs-com";
+
 export default {
-  name: "enquire"
+  name: "enquire",
+  methods: {
+    sendEmail(e) {
+      // let params = {
+      //   name: "hello",
+      //   email: "email@email.com",
+      //   message: "Message"
+      // };
+      emailjs
+        .sendForm(
+          "6079",
+          "template_cJ9DgMU9",
+          e.target,
+          "user_Ei1CA3QOgxcFGaZCxCE1M"
+        )
+        .then(result => {
+          console.log("SUCCESS", result);
+        })
+        .catch(error => {
+          console.log("ERROR", error);
+        });
+    }
+  }
 };
 </script>
 
@@ -54,6 +93,19 @@ export default {
     textarea {
       border: 2px solid #6eb4c7;
       border-radius: 30px;
+      &::placeholder {
+        color: #6eb4c7;
+        top: 40px;
+      }
+    }
+    button {
+      background-color: #70b4c2;
+      border: none;
+      color: #fff;
+      font-size: 30px;
+      border-radius: 50px;
+      width: 250px;
+      padding: 10px;
     }
   }
 }
